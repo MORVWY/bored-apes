703b252a97a167882f8efa39e3c0469c214db4f3
MORVWY <panjects@gmail.com>
2023-03-12 21:12:29 +0200

    animations

--- js/components/animations.js
+++ js/components/animations.js
@@ -1,3 +1,129 @@
-function animationRender() {}
+function animationRender() {
+  function showRightAnimation() {
+    let options = {
+      root: null,
+      rootMargin: "1px",
+      threshold: 0.01,
+    };
+
+    let callback = function (entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("slide-in-right");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    let targets = document.querySelectorAll(".anim-right");
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+
+  function showLeftAnimtaion() {
+    let options = {
+      root: null,
+      rootMargin: "1px",
+      threshold: 0.01,
+    };
+
+    let callback = function (entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("slide-in-left");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    let targets = document.querySelectorAll(".anim-left");
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+
+  function showHeaderAnimation() {
+    let options = {
+      root: null,
+      rootMargin: "1px",
+      threshold: 0.01,
+    };
+
+    let callback = function (entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("slide-in-fwd-center");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    let targets = document.querySelectorAll(".anim-header");
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+
+  function showDescriptionAnimation() {
+    let options = {
+      root: null,
+      rootMargin: "1px",
+      threshold: 0.01,
+    };
+
+    let callback = function (entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("tracking-in-expand");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    let targets = document.querySelectorAll(".anim-description");
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+
+  function showCardsAnimation() {
+    let options = {
+      root: null,
+      rootMargin: "1px",
+      threshold: 0.01,
+    };
+
+    let callback = function (entries, observer) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("scale-in-center");
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+
+    let targets = document.querySelectorAll(".anim-cards");
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+  }
+
+  showRightAnimation();
+  showLeftAnimtaion();
+  showHeaderAnimation();
+  showDescriptionAnimation();
+  showCardsAnimation();
+}
 
 export { animationRender };
