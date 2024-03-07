db93aa490ff105d8cf1bf38771f12f9c6f7e4f3f
MORVWY <panjects@gmail.com>
2024-03-07 21:58:26 +0200

    test commint gitkr

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ import { renderJsonFiles } from "./components/render.js"
 import { renderTeamMembers } from "./data/team.js"
 import { renderRoadmapCards } from "./data/roadmap.js"
 
+//test
 window.addEventListener("DOMContentLoaded", () => {
   onHamb()
   onScroll()
