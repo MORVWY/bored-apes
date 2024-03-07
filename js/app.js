eb6ca318f3f2fce66c993b07b5fc181418f8ea89
MORVWY <panjects@gmail.com>
2024-03-07 21:52:51 +0200

    changed app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
-import { onHamb } from "./components/hamburger.js";
-import { onScroll } from "./components/scroll-btn.js";
-import { animationRender } from "./components/animations.js";
-import { renderJsonFiles } from "./components/render.js";
-import { renderTeamMembers } from "./data/team.js";
-import { renderRoadmapCards } from "./data/roadmap.js";
+import { onHamb } from "./components/hamburger.js"
+import { onScroll } from "./components/scroll-btn.js"
+import { animationRender } from "./components/animations.js"
+import { renderJsonFiles } from "./components/render.js"
+import { renderTeamMembers } from "./data/team.js"
+import { renderRoadmapCards } from "./data/roadmap.js"
 
-//Content
 window.addEventListener("DOMContentLoaded", () => {
-  onHamb();
-  onScroll();
-  animationRender();
-  renderJsonFiles();
-  renderTeamMembers();
-  renderRoadmapCards();
-});
+  onHamb()
+  onScroll()
+  animationRender()
+  renderJsonFiles()
+  renderTeamMembers()
+  renderRoadmapCards()
+})
